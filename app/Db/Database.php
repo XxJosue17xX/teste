b4fbea241cb76d3d9db30cd1f89504f4e7717583
VTsilva <vs54256silva@gmail.com>
2021-11-18 14:59:33 -0300

    paginação completa

--- app/Db/Database.php
+++ app/Db/Database.php
@@ -5,156 +5,163 @@ namespace App\Db;
 use \PDO;
 use \PDOException;
 
-class Database{
+class Database
+{
 
-  /**
-   * Host de conexão com o banco de dados
-   * @var string
-   */
-  const HOST = 'localhost:3306';
+    /**
+     * Host de conexão com o banco de dados
+     * @var string
+     */
+    const HOST = 'localhost:3308';
 
-  /**
-   * Nome do banco de dados
-   * @var string
-   */
-  const NAME = 'vagas';
+    /**
+     * Nome do banco de dados
+     * @var string
+     */
+    const NAME = 'vagas';
 
-  /**
-   * Usuário do banco
-   * @var string
-   */
-  const USER = 'root';
+    /**
+     * Usuário do banco
+     * @var string
+     */
+    const USER = 'root';
 
-  /**
-   * Senha de acesso ao banco de dados
-   * @var string
-   */
-  const PASS = '';
+    /**
+     * Senha de acesso ao banco de dados
+     * @var string
+     */
+    const PASS = 'vs521733';
 
-  /**
-   * Nome da tabela a ser manipulada
-   * @var string
-   */
-  private $table;
+    /**
+     * Nome da tabela a ser manipulada
+     * @var string
+     */
+    private $table;
 
-  /**
-   * Instancia de conexão com o banco de dados
-   * @var PDO
-   */
-  private $connection;
+    /**
+     * Instancia de conexão com o banco de dados
+     * @var PDO
+     */
+    private $connection;
 
-  /**
-   * Define a tabela e instancia e conexão
-   * @param string $table
-   */
-  public function __construct($table = null){
-    $this->table = $table;
-    $this->setConnection();
-  }
-
-  /**
-   * Método responsável por criar uma conexão com o banco de dados
-   */
-  private function setConnection(){
-    try{
-      $this->connection = new PDO('mysql:host='.self::HOST.';dbname='.self::NAME,self::USER,self::PASS);
-      $this->connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    }catch(PDOException $e){
-      die('ERROR: '.$e->getMessage());
+    /**
+     * Define a tabela e instancia e conexão
+     * @param string $table
+     */
+    public function __construct($table = null)
+    {
+        $this->table = $table;
+        $this->setConnection();
     }
-  }
 
-  /**
-   * Método responsável por executar queries dentro do banco de dados
-   * @param  string $query
-   * @param  array  $params
-   * @return PDOStatement
-   */
-  public function execute($query,$params = []){
-    try{
-      $statement = $this->connection->prepare($query);
-      $statement->execute($params);
-      return $statement;
-    }catch(PDOException $e){
-      die('ERROR: '.$e->getMessage());
+    /**
+     * Método responsável por criar uma conexão com o banco de dados
+     */
+    private function setConnection()
+    {
+        try {
+            $this->connection = new PDO('mysql:host=' . self::HOST . ';dbname=' . self::NAME, self::USER, self::PASS);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('ERROR: ' . $e->getMessage());
+        }
     }
-  }
 
-  /**
-   * Método responsável por inserir dados no banco
-   * @param  array $values [ field => value ]
-   * @return integer ID inserido
-   */
-  public function insert($values){
-    //DADOS DA QUERY
-    $fields = array_keys($values);
-    $binds  = array_pad([],count($fields),'?');
+    /**
+     * Método responsável por executar queries dentro do banco de dados
+     * @param  string $query
+     * @param  array  $params
+     * @return PDOStatement
+     */
+    public function execute($query, $params = [])
+    {
+        try {
+            $statement = $this->connection->prepare($query);
+            $statement->execute($params);
+            return $statement;
+        } catch (PDOException $e) {
+            die('ERROR: ' . $e->getMessage());
+        }
+    }
 
-    //MONTA A QUERY
-    $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).') VALUES ('.implode(',',$binds).')';
+    /**
+     * Método responsável por inserir dados no banco
+     * @param  array $values [ field => value ]
+     * @return integer ID inserido
+     */
+    public function insert($values)
+    {
+        //DADOS DA QUERY
+        $fields = array_keys($values);
+        $binds  = array_pad([], count($fields), '?');
 
-    //EXECUTA O INSERT
-    $this->execute($query,array_values($values));
+        //MONTA A QUERY
+        $query = 'INSERT INTO ' . $this->table . ' (' . implode(',', $fields) . ') VALUES (' . implode(',', $binds) . ')';
 
-    //RETORNA O ID INSERIDO
-    return $this->connection->lastInsertId();
-  }
+        //EXECUTA O INSERT
+        $this->execute($query, array_values($values));
 
-  /**
-   * Método responsável por executar uma consulta no banco
-   * @param  string $where
-   * @param  string $order
-   * @param  string $limit
-   * @param  string $fields
-   * @return PDOStatement
-   */
-  public function select($where = null, $order = null, $limit = null, $fields = '*'){
-    //DADOS DA QUERY
-    $where = strlen($where) ? 'WHERE '.$where : '';
-    $order = strlen($order) ? 'ORDER BY '.$order : '';
-    $limit = strlen($limit) ? 'LIMIT '.$limit : '';
+        //RETORNA O ID INSERIDO
+        return $this->connection->lastInsertId();
+    }
 
-    //MONTA A QUERY
-    $query = 'SELECT '.$fields.' FROM '.$this->table.' '.$where.' '.$order.' '.$limit;
+    /**
+     * Método responsável por executar uma consulta no banco
+     * @param  string $where
+     * @param  string $order
+     * @param  string $limit
+     * @param  string $fields
+     * @return PDOStatement
+     */
+    public function select($where = null, $order = null, $limit = null, $fields = '*')
+    {
+        //DADOS DA QUERY
+        $where = strlen($where) ? 'WHERE ' . $where : '';
+        $order = strlen($order) ? 'ORDER BY ' . $order : '';
+        $limit = strlen($limit) ? 'LIMIT ' . $limit : '';
 
-    //EXECUTA A QUERY
-    return $this->execute($query);
-  }
+        //MONTA A QUERY
+        $query = 'SELECT ' . $fields . ' FROM ' . $this->table . ' ' . $where . ' ' . $order . ' ' . $limit;
 
-  /**
-   * Método responsável por executar atualizações no banco de dados
-   * @param  string $where
-   * @param  array $values [ field => value ]
-   * @return boolean
-   */
-  public function update($where,$values){
-    //DADOS DA QUERY
-    $fields = array_keys($values);
+        //EXECUTA A QUERY
+        return $this->execute($query);
+    }
 
-    //MONTA A QUERY
-    $query = 'UPDATE '.$this->table.' SET '.implode('=?,',$fields).'=? WHERE '.$where;
+    /**
+     * Método responsável por executar atualizações no banco de dados
+     * @param  string $where
+     * @param  array $values [ field => value ]
+     * @return boolean
+     */
+    public function update($where, $values)
+    {
+        //DADOS DA QUERY
+        $fields = array_keys($values);
 
-    //EXECUTAR A QUERY
-    $this->execute($query,array_values($values));
+        //MONTA A QUERY
+        $query = 'UPDATE ' . $this->table . ' SET ' . implode('=?,', $fields) . '=? WHERE ' . $where;
 
-    //RETORNA SUCESSO
-    return true;
-  }
+        //EXECUTAR A QUERY
+        $this->execute($query, array_values($values));
 
-  /**
-   * Método responsável por excluir dados do banco
-   * @param  string $where
-   * @return boolean
-   */
-  public function delete($where){
-    //MONTA A QUERY
-    $query = 'DELETE FROM '.$this->table.' WHERE '.$where;
+        //RETORNA SUCESSO
+        return true;
+    }
 
-    //EXECUTA A QUERY
-    $this->execute($query);
+    /**
+     * Método responsável por excluir dados do banco
+     * @param  string $where
+     * @return boolean
+     */
+    public function delete($where)
+    {
+        //MONTA A QUERY
+        $query = 'DELETE FROM ' . $this->table . ' WHERE ' . $where;
 
-    //RETORNA SUCESSO
-    return true;
-  }
+        //EXECUTA A QUERY
+        $this->execute($query);
 
+        //RETORNA SUCESSO
+        return true;
+    }
 }
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__.'/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use \App\Entity\Vaga;
 use \App\Db\Pagination;
@@ -9,13 +9,13 @@ use \App\Db\Pagination;
 $busca = filter_input(INPUT_GET, 'busca', FILTER_SANITIZE_STRING);
 
 //filtro de status
-$filtroStatus = filter_input(INPUT_GET, 'status', FILTER_SANITIZE_STRING);
-$filtroStatus = in_array($filtroStatus,['s', 'n']) ? $filtroStatus : '';
+$filtroStatus = filter_input(INPUT_GET, 'filtroStatus', FILTER_SANITIZE_STRING);
+$filtroStatus = in_array($filtroStatus, ['s', 'n']) ? $filtroStatus : '';
 
 //condiçoes sql
 $condicoes = [
-  strlen($busca) ? 'titulo LIKE "%'.str_replace(' ', '%', $busca).'%"' : null,
-  strlen($filtroStatus) ? 'ativo = "'.$filtroStatus.'"' : null  
+    strlen($busca) ? 'titulo LIKE "%' . str_replace(' ', '%', $busca) . '%"' : null,
+    strlen($filtroStatus) ? 'ativo = "' . $filtroStatus . '"' : null
 ];
 
 //remove posiçoes vazias
@@ -33,6 +33,6 @@ $obPagination = new Pagination($quantidadeVagas, $_GET['pagina'] ?? 1, 10);
 //obtem as vagas
 $vagas = Vaga::getVagas($where, null, $obPagination->getLimit());
 
-include __DIR__.'/includes/header.php';
-include __DIR__.'/includes/listagem.php';
-include __DIR__.'/includes/footer.php';
+include __DIR__ . '/includes/header.php';
+include __DIR__ . '/includes/listagem.php';
+include __DIR__ . '/includes/footer.php';
